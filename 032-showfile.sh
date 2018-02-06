f28779918c54e6a2f5b82d95bd3681b76df653f4
beautifularea <beautifularea@163.com>
2018-02-06 11:10:17 +0800

    Update 032-showfile.sh

--- 032-showfile.sh
+++ 032-showfile.sh
@@ -1,19 +1,21 @@
 #!/bin/sh
 # showfile - show the contents of a file, including additional useful info
 
-width=72
+width=80
 
 for input
 do
   lines="$(wc -l < $input | sed 's/ //g')"
   chars="$(wc -c < $input | sed 's/ //g')"
   owner="$(ls -ld $input | awk '{print $3}')"
+  
   echo "-----------------------------------------------------------------"
   echo "File $input ($lines lines, $chars characters, owned by $owner):"
   echo "-----------------------------------------------------------------"
+  
   while read line 
     do
-      if [ ${#line} -gt $width ] ; then
+      if [ ${#line} -gt $width ] ; then # ${#line} ---> line's length
         echo "$line" | fmt | sed -e '1s/^/  /' -e '2,$s/^/+ /'
       else
         echo "  $line"
