035dbcdb9079f8c6bca321f78cb37b7d875cd802
beautifularea <beautifularea@163.com>
2018-02-07 09:57:05 +0800

    FIXED

--- 013-hilow.sh
+++ 013-hilow.sh
@@ -19,3 +19,23 @@ done
 echo "Right!! Guessed $number in $guesses guesses."
 
 exit 0
+
+#$(( xx )) 整数计算
+#-ne not euqal
+#echo -n 输出，不换行
+#echo -e 输出，并换行
+#"$guess" ， 和 $guess的主要区别是，前者能把内容完整输出， 后者可能不会输出内容中的空格等特殊字符
+#-gt greater
+#-lt less
+
+#while [ ] ; do
+#done
+
+#if [ ] ; then
+#fi
+
+#if [ ] ; then
+#elif [ ] ; then
+#fi
+
+#$$ 当前进程ID
