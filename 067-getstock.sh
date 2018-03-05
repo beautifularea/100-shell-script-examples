4bf813e45101aa32a784bc9a73d2df399d26db8f
beautifularea <beautifularea@163.com>
2018-03-05 10:24:56 +0800

    Fixed

--- 067-getstock.sh
+++ 067-getstock.sh
@@ -5,15 +5,18 @@
 
 url="http://finance.lycos.com/qc/stocks/quotes.aspx?symbols="
 
-if [ $# -ne 1 ] ; then
+if [ $# -ne 1 ] ; then #not equal
   echo "Usage: $(basename $0) stocksymbol" >&2 
   exit 1
 fi
 
-value="$(lynx -dump "$url$1" | grep 'Last price:' | \
-  awk -F: 'NF > 1 && $(NF) != "N/A" { print $(NF) }')"
+#lynx www.baidu.com 文本浏览器，不支持多媒体
+value="$(lynx -dump "$url$1" | grep 'Last price:' | \ #lynx 
+  awk -F: 'NF > 1 && $(NF) != "N/A" { print $(NF) }')" #awk -F 
 
-if [ -z $value ] ; then
+#awk -F 设置分隔符。
+
+if [ -z $value ] ; then #-z 长度为0 ， 则为真
   echo "error: no value found for ticker symbol $1." >&2
   exit 1
 fi
